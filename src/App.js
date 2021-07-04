32b483def8bd9e97a1d000957fcfafd94051fc08
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-07-04 21:30:59 +0800

    move listener to mainPage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import './App.css';
 import MainPage from './Containers/MainPage';
 
 function App() {
-  let offsetStart = 0, offsetEnd = 0;
-    window.addEventListener('scroll', ()=>{
-        document.documentElement.style.setProperty('--scroll',
-        (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight))
-    }, false);
+  
   return (
     <div className="App">
       <MainPage/>
--- src/Containers/MainPage.jsx
+++ src/Containers/MainPage.jsx
@@ -1,14 +1,27 @@
 import '../App.css'
 import logo from '../Images/logo.png'
-import taiwan from '../Images/taiwan.png'
+import { useEffect, useState } from 'react'
 
 const MainPage = () => {
+    const [select, setSelect] = useState(false);
+    let offsetStart = 0, offsetEnd = 0;
+    let scroll;
+    window.addEventListener('scroll', ()=>{
+        let scroll = (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight)
+        document.documentElement.style.setProperty('--scroll',scroll)
+    }, false);
+    const handleClick = () =>{
+        if(scroll > 0.95){
+            setSelect(true)
+        }
+    }
     return(
         <>
-        <img src={logo} alt='logo' className='logo'/>
-        <div className='tw-wrap'>
-        {//<img src={taiwan} alt='taiwan' className='taiwan'/>
+        {select ? 
+        <></>:
+        <img src={logo} alt='logo' className='logo' onClick={handleClick}/>
         }
+        <div className='tw-wrap'>
         </div>
         </>
     )
