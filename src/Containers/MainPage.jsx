17d492a28d0c5879835fb7a05de17e3594f7fa56
Leafoliage <leafoliages@gmail.com>
2021-07-05 22:49:23 +0800

    [add] menu still trying to make those "buttons" on the menu work

--- src/Containers/MainPage.jsx
+++ src/Containers/MainPage.jsx
@@ -1,28 +1,37 @@
 import '../App.css'
 import logo from '../Images/logo.png'
+import menu from '../Images/menu.png'
 import { useEffect, useState } from 'react'
 
 const MainPage = () => {
     const [select, setSelect] = useState(false);
     let offsetStart = 0, offsetEnd = 0;
     let scroll;
-    window.addEventListener('scroll', ()=>{
-        let scroll = (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight)
-        document.documentElement.style.setProperty('--scroll',scroll)
+    window.addEventListener('scroll', () => {
+        scroll = (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight)
+        console.log('scroll:', scroll)
+        document.documentElement.style.setProperty('--scroll', scroll)
     }, false);
-    const handleClick = () =>{
-        if(scroll > 0.95){
+    const handleClick = () => {
+        if (scroll > 0.95) {
             setSelect(true)
         }
+        // scroll turns to undefined after this, dunno why
     }
-    return(
+    const handleClickMenu = () => {
+        setSelect(false)
+    }
+
+    return (
         <>
-        {select ? 
-        <></>:
-        <img src={logo} alt='logo' className='logo' onClick={handleClick}/>
-        }
-        <div className='tw-wrap'>
-        </div>
+            {select ?
+                <img src={menu} alt='menu' className='menu' onClick={handleClickMenu} />
+                :
+                <img src={logo} alt='logo' className='logo' onClick={handleClick} />
+            }
+            <div className='tw-wrap'>
+            </div>
+
         </>
     )
 }
