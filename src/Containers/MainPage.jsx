6c3b368d7e5abff59583e7009f76505c6ba66799
Leafoliage <leafoliages@gmail.com>
2021-07-06 21:42:03 +0800

    [fix] menu buttons

--- src/Containers/MainPage.jsx
+++ src/Containers/MainPage.jsx
@@ -20,12 +20,13 @@ const MainPage = () => {
     }
 
     const handleClickMenu = (e) => {
-        let x = e.clientX, y = e.clientY
-        if (x>983 && x<1047 && y>126 && y<148) console.log("確診資訊")
-        else if (x>932 && x<996 && y>174 && y<195) console.log("死亡資訊")
-        else if (x>913 && x<974 && y>239 && y<259) console.log("醫療量能")
-        else if (x>929 && x<992 && y>308 && y<328) console.log("疫苗資訊")
-        else if (x>991 && x<1055 && y>364 && y<383) console.log("關於我們")
+        let x = Math.round((e.clientX - e.target.offsetLeft) / e.target.clientWidth * 100)
+        let y = Math.round((e.clientY - e.target.offsetTop) / e.target.clientHeight * 100)
+        if (x>49 && x<77 && y>25 && y<29) console.log("確診資訊")
+        else if (x>26 && x<54 && y>33 && y<38) console.log("死亡資訊")
+        else if (x>16 && x<45 && y>45 && y<51) console.log("醫療量能")
+        else if (x>24 && x<52 && y>59 && y<64) console.log("疫苗資訊")
+        else if (x>53 && x<81 && y>69 && y<75) console.log("關於我們")
         else setSelect(false)
     }
 
