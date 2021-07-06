f2c73526a504a6ea7d85cf63d86f2af7f93fb107
Leafoliage <leafoliages@gmail.com>
2021-07-06 20:55:37 +0800

    [add] menu function

--- src/Containers/MainPage.jsx
+++ src/Containers/MainPage.jsx
@@ -12,14 +12,22 @@ const MainPage = () => {
         console.log('scroll:', scroll)
         document.documentElement.style.setProperty('--scroll', scroll)
     }, false);
+    
     const handleClick = () => {
         if (scroll > 0.95) {
             setSelect(true)
         }
         // scroll turns to undefined after this, dunno why
     }
-    const handleClickMenu = () => {
-        setSelect(false)
+
+    const handleClickMenu = (e) => {
+        let x = e.clientX, y = e.clientY
+        if (x>983 && x<1047 && y>126 && y<148) console.log("確診資訊")
+        else if (x>932 && x<996 && y>174 && y<195) console.log("死亡資訊")
+        else if (x>913 && x<974 && y>239 && y<259) console.log("醫療量能")
+        else if (x>929 && x<992 && y>308 && y<328) console.log("疫苗資訊")
+        else if (x>991 && x<1055 && y>364 && y<383) console.log("關於我們")
+        else setSelect(false)
     }
 
     return (
