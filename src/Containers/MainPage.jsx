320955f24a40578ffa3ab108bf1490e64b6cf1d3
Leafoliage <leafoliages@gmail.com>
2021-07-06 20:59:41 +0800

    [modify] make scroll a state

--- src/Containers/MainPage.jsx
+++ src/Containers/MainPage.jsx
@@ -1,23 +1,22 @@
 import '../App.css'
 import logo from '../Images/logo.png'
 import menu from '../Images/menu.png'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const MainPage = () => {
     const [select, setSelect] = useState(false);
+    const [scroll, setScroll] = useState(0)
     let offsetStart = 0, offsetEnd = 0;
-    let scroll;
+
     window.addEventListener('scroll', () => {
-        scroll = (window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight)
-        console.log('scroll:', scroll)
+        setScroll((window.pageYOffset - offsetStart) / (document.body.offsetHeight - offsetStart - offsetEnd - window.innerHeight))
         document.documentElement.style.setProperty('--scroll', scroll)
     }, false);
-    
+
     const handleClick = () => {
         if (scroll > 0.95) {
             setSelect(true)
         }
-        // scroll turns to undefined after this, dunno why
     }
 
     const handleClickMenu = (e) => {
@@ -39,7 +38,6 @@ const MainPage = () => {
             }
             <div className='tw-wrap'>
             </div>
-
         </>
     )
 }
