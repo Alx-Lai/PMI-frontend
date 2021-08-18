86c01daeb0d5c9b95744160edd66fd33df9d1717
Leafoliage <leafoliages@gmail.com>
2021-08-18 22:50:46 +0800

    [add] page pendamic

---
+++ src/Containers/Pendamic.jsx
@@ -0,0 +1,41 @@
+import "./Pendamic.css"
+
+export default function Pandemic() {
+    const caseForeign = 2, caseDomestic = 8, caseTotal = 15862, deathToday = 0, deathTotal = 821
+    const vaccinatedToday = 64293, vaccinatedTotal = 9824837, vaccinatedPercent = 39.04
+    const vacGold = "連江縣", vacSilver = "澎湖縣", vacBronze = "台北市"
+    
+    return (
+        <div id='wrap'>
+            <div class='sec'>
+                <h3>全台今日新增確診</h3>
+                <h2>境外: +{caseForeign}</h2>
+                <h2>境內: +{caseDomestic}</h2>
+            </div>
+            <div class='sec'>
+                <h3>累積染疫人數</h3>
+                <h2>{caseTotal}人</h2>
+            </div>
+            <div class='sec'>
+                <h3>今日新增死亡</h3>
+                <h2>+{deathToday}</h2>
+                <h3>累積死亡人數</h3>
+                <h2>{deathTotal}人</h2>
+            </div>
+            <div class='sec'>
+                <h3>今日疫苗施打人數</h3>
+                <h2>+{vaccinatedToday}</h2>
+                <h3>累積施打人數</h3>
+                <h2>{vaccinatedTotal}人</h2>
+            </div>
+            <div class='sec'>
+                <h3>台灣疫苗施打率</h3>
+                <h2>{vaccinatedPercent}%</h2>
+                <h3>施打排行:</h3>
+                <h2>No.1 {vacGold}</h2>
+                <h2>No.2 {vacSilver}</h2>
+                <h2>No.3 {vacBronze}</h2>
+            </div>
+        </div>
+    )
+};
