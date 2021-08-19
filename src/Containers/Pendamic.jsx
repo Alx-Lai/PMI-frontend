33c6523a37317f73a9c02ffe9a4f2ddb19a51a41
Leafoliage <leafoliages@gmail.com>
2021-08-19 22:22:41 +0800

    [modify] page pendamic

--- src/Containers/Pendamic.jsx
+++ src/Containers/Pendamic.jsx
@@ -9,32 +9,32 @@ export default function Pandemic() {
         <div id='wrap'>
             <div class='sec'>
                 <h3>全台今日新增確診</h3>
-                <h2>境外: +{caseForeign}</h2>
-                <h2>境內: +{caseDomestic}</h2>
+                <h2 class='pendamic-item'>境外: +{caseForeign}</h2>
+                <h2 class='pendamic-item'>境內: +{caseDomestic}</h2>
             </div>
             <div class='sec'>
                 <h3>累積染疫人數</h3>
-                <h2>{caseTotal}人</h2>
+                <h2 class='pendamic-item'>{caseTotal}人</h2>
             </div>
             <div class='sec'>
                 <h3>今日新增死亡</h3>
-                <h2>+{deathToday}</h2>
+                <h2 class='pendamic-item'>+{deathToday}</h2>
                 <h3>累積死亡人數</h3>
-                <h2>{deathTotal}人</h2>
+                <h2 class='pendamic-item'>{deathTotal}人</h2>
             </div>
             <div class='sec'>
                 <h3>今日疫苗施打人數</h3>
-                <h2>+{vaccinatedToday}</h2>
+                <h2 class='pendamic-item'>+{vaccinatedToday}</h2>
                 <h3>累積施打人數</h3>
-                <h2>{vaccinatedTotal}人</h2>
+                <h2 class='pendamic-item'>{vaccinatedTotal}人</h2>
             </div>
             <div class='sec'>
                 <h3>台灣疫苗施打率</h3>
-                <h2>{vaccinatedPercent}%</h2>
+                <h2 class='pendamic-item'>{vaccinatedPercent}%</h2>
                 <h3>施打排行:</h3>
-                <h2>No.1 {vacGold}</h2>
-                <h2>No.2 {vacSilver}</h2>
-                <h2>No.3 {vacBronze}</h2>
+                <h2 class='pendamic-item'>No.1 {vacGold}</h2>
+                <h2 class='pendamic-item'>No.2 {vacSilver}</h2>
+                <h2 class='pendamic-item'>No.3 {vacBronze}</h2>
             </div>
         </div>
     )
