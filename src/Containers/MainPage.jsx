a3372ef761ce50371569af977b2281b522569757
Leafoliage <leafoliages@gmail.com>
2021-08-31 18:51:44 +0800

    [modify] logo & menu function

--- src/Containers/MainPage.jsx
+++ src/Containers/MainPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import '../App.css'
 import './MainPage.css'
 import logo from '../Images/logo.png'
@@ -5,20 +6,22 @@ import taiwan from '../Images/taiwan.png'
 import Menu from './Menu'
 
 const MainPage = ({ scroll }) => {
+    const [clicked, setClicked] = useState(false)
+
+    const clickLogo = () => {
+        setClicked(true)
+    }
 
     return (
         <div className='mainpage'>
-            <Menu />
+            <Menu hidden={!clicked} />
             <div className='bed' hidden={scroll < 0.9}>
                 <h2 className='bed-title1'>病床資訊</h2>
                 <input className='bed-input' placeholder='醫院名稱'></input>
                 <h2 className='bed-title2'>點擊區域查看分區病床資訊</h2>
             </div>
-
-            <div>
-                <img src={logo} alt='logo' className='logo' />
-                <img alt='taiwan' id='taiwan' src={taiwan} />
-            </div>
+            <img src={logo} alt='logo' className='logo' hidden={clicked} onClick={clickLogo} />
+            <img src={taiwan} alt='taiwan' id='taiwan' />
         </div>
     )
 }
--- src/Containers/Menu.jsx
+++ src/Containers/Menu.jsx
@@ -1,22 +1,22 @@
 import menu from '../Images/menu.png'
 import './Menu.css'
 
-export default function Menu() {
+export default function Menu({ hidden = false }) {
 
-    const handleClickMenu = (e) => {
-      let x = Math.round((e.clientX - e.target.offsetLeft) / e.target.clientWidth * 100)
-      let y = Math.round((e.clientY - e.target.offsetTop) / e.target.clientHeight * 100)
-      if (x > 49 && x < 88 && y > 25 && y < 29) window.location.href = "pendamic"
-      else if (x > 16 && x < 55 && y > 41 && y < 45) window.location.href = "hospitals"
-      else if (x > 28 && x < 43 && y > 64 && y < 66) console.log("疫苗分類")
-      else if (x > 28 && x < 50 && y >= 67 && y <= 68) console.log("Q & A")
-      else if (x > 28 && x < 43 && y >= 70 && y <= 71) console.log("文獻資料")
-      else if (x > 52 && x < 83 && y > 70 && y < 75) window.location.href = "aboutus"
-      else if (x > 82 && x < 98 && y > 67 && y < 74) window.location.href = "aboutus"
-      else window.location.href = "/"
-    }
+  const handleClickMenu = (e) => {
+    let x = Math.round((e.clientX - e.target.offsetLeft) / e.target.clientWidth * 100)
+    let y = Math.round((e.clientY - e.target.offsetTop) / e.target.clientHeight * 100)
+    if (x > 49 && x < 88 && y > 25 && y < 29) window.location.href = "pendamic"
+    else if (x > 16 && x < 55 && y > 41 && y < 45) window.location.href = "hospitals"
+    else if (x > 28 && x < 43 && y > 64 && y < 66) console.log("疫苗分類")
+    else if (x > 28 && x < 50 && y >= 67 && y <= 68) console.log("Q & A")
+    else if (x > 28 && x < 43 && y >= 70 && y <= 71) console.log("文獻資料")
+    else if (x > 52 && x < 83 && y > 70 && y < 75) window.location.href = "aboutus"
+    else if (x > 82 && x < 98 && y > 67 && y < 74) window.location.href = "aboutus"
+    else window.location.href = "/"
+  }
 
-    return (
-        <img src={menu} alt='menu' className='menu' onClick={handleClickMenu} />
-    )
+  return (
+    <img src={menu} alt='menu' className='menu' onClick={handleClickMenu} hidden={hidden} />
+  )
 };
